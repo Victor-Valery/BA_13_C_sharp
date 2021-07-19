ce7c32f03f87d7c4b00928151764af71125affcb
Victor-Valery <81595723+Victor-Valery@users.noreply.github.com>
2021-07-19 16:13:24 +0200

    Class_structures

--- Program.cs
+++ Program.cs
@@ -2,19 +2,53 @@
 
 namespace BA_13_C_sharp
 {
+    public class Car
+    {
+        public string Brand { get; set; }
+        public string Model { get; set; }
 
+        private int speed;
 
-
-
-
-
-
+        public int Speed
+        {
+            get { return speed; }
+            set
+            {
+                if (value > 300)
+                {
+                    Console.WriteLine($"YANLIS DEGER {value}");
+                    speed = 0;
+                }
+                else
+                {
+                    speed = value;
+                }
+            }
+        }
+        public void P()
+        {
+            Console.WriteLine($"MARKA: {this.Brand}\nMODEL: {this.Model}\nHIZ: {this.speed}");
+        }
+    }
 
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Car c1 = new Car();
+            c1.Brand = "BMW";
+            c1.Model = "520";
+            c1.Speed = 305;
+            c1.P();
+
+            Console.WriteLine("********************************");
+
+            Car c2 = new Car();
+            c2.Brand = "AUDI";
+            c2.Model = "S5";
+            c2.Speed = 250;
+            c2.P();
+
         }
     }
 }
